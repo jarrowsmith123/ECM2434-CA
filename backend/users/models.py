69c5bb368ab56a622cb5a6465b4f89c9e28f6724
jarrowsmith123 <ja774@exeter.ac.uk>
2025-03-05 18:14:02 +0000

    added friend model

--- backend/users/models.py
+++ backend/users/models.py
@@ -23,8 +23,14 @@ def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
 
+class Friendship(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_friend_requests')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_friend_requests')
+    status = models.CharField(max_length=10, choices=[('pending', 'Pending'), ('accepted', 'Accepted'), ('rejected', 'Rejected')], default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
     
-
+    def __str__(self):
+        return f"Friend Request: {self.sender.username} → {self.receiver.username} ({self.status})"
 
     
 
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import UserProfile
+from .models import UserProfile, Friendship
 
 class UserProfileSerializer(serializers.ModelSerializer):
 
@@ -32,5 +32,7 @@ class UserSerializer(serializers.ModelSerializer):
             
         instance.save()
         return instance
+
+
 
 
